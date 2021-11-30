517d5d2abb0ac2d4aa1e0133c88471802cda010b
Tara Gallagher <tgallagh@ui.edu>
2021-11-30 12:05:05 -0700

    spectral data

--- Code/py/Spectral_fig1.py
+++ Code/py/Spectral_fig1.py
@@ -18,71 +18,47 @@ import pandas as pd
 import seaborn as sns
 
 
+# grab lsm files
+# 
+dyes_dir = '/Users/Tara/Documents/GitHub/PseudomonasFLIM/Data/Spectral/Dyes/'
+output_dir = '/Users/Tara/Documents/GitHub/PseudomonasFLIM/Output/'
+semrock_path = '/Users/Tara/Documents/GitHub/PseudomonasFLIM/Data/Microscope/emission_filters/semrock_filter.csv'
 
-start=410
-end=695
-
-
-
-
-
-
-wavelen=np.linspace(410,695,32)  # make list of wavelengths for channels 1-32
-
-
-spectral_dir = ("/Users/sltg/Documents/GitHub/PseudomonasFLIM/Data/Spectral/")
-
-
-# for loop
-background=tiff.imread(spectral_dir + "pyo_tcep_gradients/Blank_blank1mMTCEPblank_20p_68fov_spectral_63xOil_740nm_410-695_9nmstep_4usdwell_bidi16bitlinesum.lsm")
-
-
-background = np.sum(background[0,0,:,:,:],2)
-background=np.sum(background,1)[0:32]
-background=pd.DataFrame(background)
-background=background.rename(columns={0:"Intensity"})
-background.insert(1, "Normalized", background.Intensity/sum(background.Intensity))
-
-
-
-spec_df=pd.DataFrame()
-spec_df=pd.DataFrame()
-
-for file in os.listdir():
-    image_stack=tiff.imread(file)
-    spec = np.sum(image_stack[0,0,:,:,:],2) 
-    spec1=np.sum(spec,1)[0:32] # 32 channels (last channel is Transmission)
-    spec1=pd.DataFrame(spec1)
-    spec1.insert(0, "FileName", file)
-    spec1=spec1.rename(columns={0:"Intensity"})
-    spec1.insert(2, "Normalized", spec1.Intensity/max(spec1.Intensity))
-    spec_df=spec1.append(spec_df)
+spec_df = pd.DataFrame()
+for file in os.listdir(dyes_dir):
+    if 'lsm' in file:
+        path = dyes_dir + file
+        df = lsmfiles.get_lsm(path)
+        spectra, wavelen = lsmfiles.channels(path)
+        df = lsmfiles.frame_sum(df)
+        df = lsmfiles.scale(df, wavelen)
+        df.insert(0, "FileName", file)
+        spec_df = spec_df.append(df)
+        
 
 #spec_df.FileName.astype("category")
 spec_df.insert(0,"Sample",spec_df.FileName)
-spec_df.Sample[spec_df.Sample.str.contains("PCH")]="Pyochelin"
+spec_df.Sample[spec_df.Sample.str.contains("PCH")]="PCH"
 spec_df.Sample[spec_df.Sample.str.contains("NADH")]="Free NADH"
-spec_df.Sample[spec_df.Sample.str.contains("PYO")]="Reduced Pyocyanin"
+spec_df.Sample[spec_df.Sample.str.contains("PYO")]="Reduced PYO"
 spec_df.Sample[spec_df.Sample.str.contains("CPX")]="CPX"
 spec_df.Sample[spec_df.Sample.str.contains("FAD")]="FAD"
-spec_df.Sample[spec_df.Sample.str.contains("PVD")]="Pyoverdine"
+spec_df.Sample[spec_df.Sample.str.contains("PVD")]="PVD"
+spec_df.Sample[spec_df.Sample.str.contains("OHPhz")]="Reduced OHPhz"
 
-semrock_filter=pd.read_csv("/Volumes/GoogleDrive/My Drive/PseudomonasImaging/Data/Spectral/semrock_filter.csv")
-semrock_filter.insert(2, "Normalized", semrock_filter.Intensity/max(semrock_filter.Intensity))
+semrock_filter=pd.read_csv(semrock_path)
+semrock_filter.insert(2, "Normalized", semrock_filter.Intensity/sum(semrock_filter.Intensity))
 semrock_filter=semrock_filter.drop(columns="Wavelength")
 semrock_filter.insert(0, "Wavelength", wavelen)
-spec_df.insert(4, "Wavelength",pd.Series(wavelen))
 semrock_filter.Wavelength.astype("int")
 
 
-
 # Set figure size with matplotlib
-current_palette = sns.color_palette("Paired", 5)
+current_palette = sns.color_palette("Paired", 10)
 plt.figure(figsize=(10,6))
-
 plt.stackplot(semrock_filter["Wavelength"], semrock_filter["Normalized"], alpha=0.3, colors=["#999999"])
-sns.lineplot(x="Wavelength", y="Normalized", hue="Sample", style="Sample", data=spec_df[spec_df.Sample!="Pyochelin"])
-
+sns.lineplot(x="Wavelength", y="Normalized", hue="Sample", data=spec_df[spec_df.Sample!="Pyochelin"])
+plt.savefig(output_dir+'fig2.pdf', dpi=300)
  
 
 
--- Code/py/utils/lsmfiles.py
+++ Code/py/utils/lsmfiles.py
@@ -33,26 +33,99 @@ def get_lsm(path):
     """
     if os.path.exists(path):
         df = tiff.imread(path)
+        return df
     else:
         raise Exception(f"Does not exist: {path}")
 
 
-def channels(df):
+def channels(path):
     """
-    Get number of channels from lsm df
+    Get channels from lsm file
 
     Parameters
     ----------
-    df : <str>
-        df of spectral lsm
+    path : <str>
+        path to lsm
 
     Returns
     -------
-    <int> number of channels
+    channels: <list of strs> 
+        channels obbject 
+    wavelen: <list of ints> 
+        spectral wavelengths
 
     """
-    if len(df.shape) == 5:
-        channels = df.shape[2]
-    else:
-        raise Exception('Number of dimensions unexpected for spectral lsm!')
+    tif =  tiff.TiffFile(path)
+    channels = tif.lsm_metadata['ChannelColors']['ColorNames']
+    wavelen = list()
+    for x in channels:
+        try: 
+            int(x)
+            wavelen.append(int(x))
+        except:
+            pass
+    return channels, wavelen
     
+    
+def frame_sum(df):
+    """
+    Get channels from lsm file
+
+    Parameters
+    ----------
+    df : <numpy array>
+        Df of lsm data
+    
+    channels: <list>
+        list of ints of wavelengths 
+        
+
+    Returns
+    -------
+    df <numpy array>
+        collapsed to get sum across entire image frame
+
+    """
+    # sum across
+    df = np.sum(df[0,0,:,:,:],2)
+    # sum down 
+    df = np.sum(df, 1)
+    return df 
+
+
+def scale(df, wavelen):
+    """
+    scale spectral data to get fraction out of 1 
+    
+    
+    Parameters
+    ----------
+    df : <numpy array>
+        df of spectral data summed across frame
+    
+    channels: <list>
+        list of channels from lsm
+
+    Returns
+    -------
+    df <pandas df>
+        normalized to 1 
+    
+    
+    """
+    # keep fl. measurements for wavelengths
+    df = df[0:len(wavelen)]
+    df = pd.DataFrame(df)
+    df = df.rename(columns={0:"Intensity"})
+    df.insert(1, "Normalized", df.Intensity/sum(df.Intensity))
+    df.insert(0, "Wavelength", wavelen)
+    return df
+
+
+
+    
+    
+
+    
+
+
